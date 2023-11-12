a6cae767e87f9ac08573e899615ecf5bceb72b84
kasparov <32327548+1F47E@users.noreply.github.com>
2023-11-12 15:25:46 -0300

    validator

--- internal/api/api_dto.go
+++ internal/api/api_dto.go
@@ -1,11 +1,17 @@
 package api
 
+import "github.com/go-playground/validator/v10"
+
 type TransactionDTO struct {
-	Hash    string `json:"hash"`
-	Account string `json:"account"`
-	Success bool   `json:"success"`
-	// LogicalTime uint64 `json:"logical_time"`
-	Datetime string `json:"datetime"`
-	TotalFee string `json:"total_fee"`
+	Hash     string `json:"hash" validate:"required"`
+	Account  string `json:"account" validate:"required"`
+	Success  bool   `json:"success" validate:"required"`
+	Datetime string `json:"datetime" validate:"required"`
+	TotalFee string `json:"total_fee" validate:"required,gte=0"`
 	Comment  string `json:"comment"`
+}
+
+func (t *TransactionDTO) Validate() error {
+	validate := validator.New()
+	return validate.Struct(t)
 }
--- internal/core/core.go
+++ internal/core/core.go
@@ -4,5 +4,11 @@ package core
 func remapTXfromEntityToDTO(entityTXmodel) dtomodel, err {
 
 	dtomodel.Datetime = time.FromUnix(entityTXmodel.LogicalTime, 0).Format("2006-01-02 15:04:05")
+
 	// .validate here as well
+
+
+
+	err := dtoModel.Validate()
+
 }
